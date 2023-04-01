90c4f47a4269741cb6b4a2e60d9a2648e8fb4e45
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2023-04-01 22:50:56 +0600

    created homepage container & component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import './App.css';
+import { HomePage } from './app/containers/HomePage/HomePage';
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `}
+`
 
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold">
-                Hello world!
-            </h1>
-        </div>
+        <AppContainer>
+            <HomePage />
+        </AppContainer>
     );
 }
 
